24505fff1c0863e1bccf11024b6f8fd87f84b4a6
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-12-01 10:10:05 +0700

    :beers:

---
+++ desktop/src/com/srakrn/dinojump/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.srakrn.dinojump.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.srakrn.dinojump.DinoJumpGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DinoJumpGame(), config);
+	}
+}
