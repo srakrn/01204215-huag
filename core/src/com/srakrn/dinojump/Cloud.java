31119631bfa4ed9d1e8b88b36ca02bc91afe4376
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-12-17 10:22:46 +0700

    :sparkle: Clouds class

--- core/src/com/srakrn/dinojump/Cloud.java
+++ core/src/com/srakrn/dinojump/Cloud.java
@@ -11,41 +11,38 @@ public class Cloud {
 	{
 		add((float) 0); 
 	}};
-	public static float speed = Preferences.INITIAL_CACTUS_VELOCITY;
+	private static ArrayList<Integer> cloudsPosition = new ArrayList<Integer>();
+
+	public static float speed = 300;
 	public static float time = 0;
 	
 	public static void spawn() {
-		cactiTime.add((float) (Math.random()*(Preferences.CACTUS_TIME_GAP)+(Preferences.CACTUS_MIN_TIME) + cactiTime.get(cactiTime.size()-1)));
+		cloudsTime.add((float) (Math.random()*(2)+1 + cloudsTime.get(cloudsTime.size()-1)));
 	}
 	public static void spawnWithCustomTime(float time) {
-		cactiTime.add(time);
+		cloudsTime.add(time);
 	}
 	public static void update(float delta) {
 		time += delta;
-		while(cactiTime.size() < 5) {
+		while(cloudsTime.size() < 5) {
 			Cactus.spawn();
 		}
-		cactiDelta.clear();
-		for(int i=0; i<cactiTime.size(); i++) {
-			cactiDelta.add(cactiTime.get(i)-Cactus.time);
+		cloudsPosition.clear();
+		for(int i=0; i<cloudsTime.size(); i++) {
+			cloudsPosition.add(Math.round((cloudsTime.get(i)-Cactus.time)*speed)+50);
 		}
-		cactiPosition.clear();
-		for(int i=0; i<cactiTime.size(); i++) {
-			cactiPosition.add(Math.round((cactiTime.get(i)-Cactus.time)*speed)+50);
-		}
-		for(int i=0; i<cactiTime.size(); i++) {
-			if(cactiDelta.get(i) < -1) {
-				cactiTime.remove(i);
-				cactiDelta.remove(i);
-				cactiPosition.remove(i);
+		for(int i=0; i<cloudsTime.size(); i++) {
+			if(cloudsPosition.get(i) < -10) {
+				cloudsTime.remove(i);
+				cloudsPosition.remove(i);
 			}
 		}
 		speed += Preferences.CACTUS_ACCELERATION;
 	}
 	public static ArrayList<Float> getCactiTime() {
-		return cactiTime;
+		return cloudsTime;
 	}
 	public static ArrayList<Integer> getCactiPosition() {
-		return cactiPosition;
+		return cloudsPosition;
 	}
 }
